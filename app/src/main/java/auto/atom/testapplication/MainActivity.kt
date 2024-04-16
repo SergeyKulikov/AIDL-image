008ec71a78d6a967988547e31743f127939d2c6d
Dmitrii <Dmitriy.Subbotenko@atom.team>
2024-04-16 10:01:20 +0300

    answer

--- app/src/main/java/auto/atom/testapplication/MainActivity.kt
+++ app/src/main/java/auto/atom/testapplication/MainActivity.kt
@@ -1,40 +1,77 @@
 package auto.atom.testapplication
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import auto.atom.speedometer.service.ISpeedometerService
+import auto.atom.speedometer.service.ISpeedometerServiceCallback
 import auto.atom.testapplication.ui.theme.MyApplicationTheme
-import kotlinx.coroutines.delay
+
 
 class MainActivity : ComponentActivity() {
+    private  val TAG = "MainActivity"
+    val viewModel by viewModels<SpeedometerViewModel>()
+
+    private var callback= object : ISpeedometerServiceCallback.Stub(){
+        override fun onSpeedChanged(speed: Float) {
+            viewModel.currentSpeed.value = speed
+        }
+    }
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, iBinder: IBinder?) {
+            Log.d(TAG, "onServiceConnected() called with: name = $name, iBinder = $iBinder")
+
+            ISpeedometerService.Stub.asInterface(iBinder).setCallback(callback)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {}
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val serviceIntent = Intent()
+        serviceIntent.component = ComponentName("auto.atom.testsetvice", "auto.atom.testsetvice.SpeedometerService")
+
+
+        startForegroundService(serviceIntent)
+        bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE)
+        Log.d(TAG, "onCreate() called with: savedInstanceState = $savedInstanceState")
+
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
 
-                val viewModel = SpeedometerViewModel()
-                LaunchedEffect(Unit) {// TODO: Remowe this stub
-                    while (true){
-                        delay(1000)
-                        viewModel.currentSpeed.value = (Math.random()*180).toFloat()
-                    }
 
-                }
+//                LaunchedEffect(Unit) {// TODO: Remowe this stub
+//                    while (true){
+//                        delay(1000)
+//                        viewModel.currentSpeed.value = (Math.random()*180).toFloat()
+//                    }
+//
+//                }
 
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
                     Speedometer(speedometerViewModel = viewModel)
                 }
 
             }
         }
+
+
     }
 }
 
--- testsetvice/src/main/java/auto/atom/testsetvice/SpeedometerService.kt
+++ testsetvice/src/main/java/auto/atom/testsetvice/SpeedometerService.kt
@@ -1,9 +1,12 @@
 package auto.atom.testsetvice
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.content.pm.ServiceInfo
 import android.os.IBinder
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import auto.atom.speedometer.service.ISpeedometerService
 import auto.atom.speedometer.service.ISpeedometerServiceCallback
@@ -14,24 +17,30 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SpeedometerService : Service() {
+    private val TAG = "SpeedometerService"
 
     private val serviceScope = CoroutineScope(Dispatchers.IO)
     private var callback:ISpeedometerServiceCallback? = null
+    private val binder = object : ISpeedometerService.Stub() {
+        override fun setCallback(callback: ISpeedometerServiceCallback?) {
+            this@SpeedometerService.callback = callback
+        }
+    }
 
-
-    override fun onBind(intent: Intent): IBinder =  TODO()
+    override fun onBind(intent: Intent): IBinder = binder
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+
         startForeground()
 
         serviceScope.launch {
             while (true){
                 delay(1000)
                 callback?.onSpeedChanged((Math.random()*180).toFloat())
+                Log.d(TAG, "onSpeedChanged() called")
             }
-
         }
-
         return START_STICKY
     }
 
@@ -42,7 +51,26 @@ class SpeedometerService : Service() {
     }
 
     private fun startForeground() {
-        TODO()
+
+        runCatching {
+            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("ddd")
+                .setContentText("ddd")
+                .build()
+            startForeground(NOTIFICATION_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+        }.onFailure { it.printStackTrace() }
+    }
+
+
+    val CHANNEL_ID = "MyForegroundServiceChannel"
+    val CHANNEL_NAME = "Channel name"
+    val NOTIFICATION_ID = 1
+
+    fun createNotificationChannel() {
+        val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(notificationChannel)
     }
 
 
